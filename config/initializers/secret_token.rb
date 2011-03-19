a38588e9742058471167c65744d085b81ee7bc27
CPSC 473 Student <cs473@rails3.(none)>
2011-03-19 11:43:24 -0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'aca634a3793fc68ffb10bcfad16a4a20c9243ae91998306f9a49530b863cc9503256006002fa0633d4aa67751300dd6901241f754091bbe43afce8d7fa637037'
